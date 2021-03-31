c1c9703573a2ce4c3e0a029013fa33e43d963f80
Remy Vila <josephvila123@gmail.com>
2021-03-31 19:15:34 -0400

    dghdhdh

--- knexfile.js
+++ knexfile.js
@@ -1,52 +1,26 @@
-// Update with your config settings.
+require('dotenv').config()
 const pg = require('pg')
-
-if(process.env.DATABASE_URL) {
-  pg.defaults.ssl - { rejectUnauthorized: false }
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = { rejectUnauthorized: false }
 }
-
 const sharedConfig = {
-  client:'pg',
-  migrations: { directory: './migrations' },
-  seed: { directory: './seeds' }
+  client:'pg' ,
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
 }
-
 module.exports = {
-
   development: {
+    ...sharedConfig, 
+    connection: process.env.DEV_DATABASE_URL, 
+  },
+  testing: {
     ...sharedConfig,
-    connection: process.env.DEV_DATABASE_URL
+    connection: process.env.TESTING_DATABASE_URL,
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'dev',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-};
+    ...sharedConfig,
+    connection: process.env.DATABASE_URL,
+    // ssl: { rejectUnauthorized: false },
+    pool: { min: 2, max: 10 }
+  },
+}
